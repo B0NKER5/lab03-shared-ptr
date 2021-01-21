142426fcbcf3a7bfc572dc3fa3c6e9d2e97cdb40
b0nker <melnivlad01@gmail.com>
2021-01-21 13:39:52 +0300

    Hi

---
+++ include/SharedPtr.hpp
@@ -0,0 +1,124 @@
+// Copyright 2021 B0NKER5 <vladmelnikov1810@yandex.ru>
+
+#ifndef INCLUDE_SHAREDPTR_HPP_
+#define INCLUDE_SHAREDPTR_HPP_
+
+#include <string>
+#include <atomic>
+#include <utility>
+
+using std :: atomic;
+
+template <typename T>
+struct Counter {
+ public:
+  explicit Counter(T* p) : ptr(p), counter(1) {}
+
+  void AddCounter() {++counter;}
+
+  void Clean() {
+    if (!--counter) {
+      ptr = nullptr;
+      delete this;
+    }
+  }
+
+  size_t get() {return counter;}
+
+ private:
+  T *ptr;
+
+  atomic<size_t> counter;
+};
+
+template <typename T>
+class SharedPtr {
+ public:
+  SharedPtr() {
+    ptr = nullptr;
+    counter = new Counter<T>(nullptr);
+  }
+
+  explicit SharedPtr(T* p) : ptr(p), counter(new Counter<T>(p)) {}
+
+  SharedPtr(const SharedPtr& r) {
+    if (std::is_move_constructible<T>::value) {
+      ptr = r.ptr;
+      counter = r.counter;
+      counter->AddCounter();
+    } else {
+      throw std::runtime_error("Type not constructible.");
+    }
+  }
+
+  SharedPtr(SharedPtr&& r) {
+    if (std::is_move_assignable<T>::value) {
+      std::swap(ptr, r.ptr);
+      std::swap(counter, r.counter);
+    } else {
+      throw std::runtime_error("Type not assignable.");
+    }
+  }
+
+  ~SharedPtr() {
+    counter->Clean();
+  }
+
+  auto operator= (const SharedPtr& r) -> SharedPtr& {
+    if (std::is_move_constructible<T>::value && &r != this) {
+      counter->Clean();
+      ptr = r.ptr;
+      counter = r.counter;
+      counter->AddCounter();
+    } else if (&r == this) {
+      std::cout << "Object equal to this\n";
+    } else {
+      throw std::runtime_error("Type not constructible");
+    }
+    return *this;
+  }
+
+  auto operator= (SharedPtr&& r) -> SharedPtr& {
+    if (std::is_move_assignable<T>::value && &r != this) {
+      std::swap(ptr, r.ptr);
+      std::swap(counter, r.counter);
+    } else if (&r == this) {
+      std::cout << "Object equal to this\n";
+    } else {
+      throw std::runtime_error("Type not assignable.");
+    }
+    return *this;
+  }
+
+  explicit operator bool() const {
+    return (ptr != nullptr);
+  }
+
+  auto operator*() const -> T& {return *ptr;}
+
+  auto operator->() const -> T* {return ptr;}
+
+  auto get() -> T* {return ptr;}
+
+  void swap(SharedPtr& r) {
+    std::swap(ptr, r.ptr);
+    std::swap(counter, r.counter);
+  }
+
+  auto use_count() const -> size_t {
+    if (ptr != nullptr){
+      return counter->get();
+    } else {
+      return 0;
+    }
+  }
+
+ private:
+  T *ptr;
+
+  Counter<T>* counter;
+};
+
+
+
+#endif // INCLUDE_SHAREDPTR_HPP_
---
+++ tests/SharedPtr_test.cpp
@@ -0,0 +1,201 @@
+// Copyright 2021 B0NKER5 <vladmelnikov1810@yandex.ru>
+
+#include <gtest/gtest.h>
+#include <SharedPtr.hpp>
+
+TEST(Example, EmptyTest) {
+  EXPECT_TRUE(true);
+}
+
+TEST(EmptyConstructer, Nullptr) {
+  SharedPtr<int> test;
+  EXPECT_EQ(test.get(), nullptr);
+  EXPECT_EQ(test.use_count(), 0);
+  EXPECT_FALSE(test);
+}
+
+TEST(Constructers, Constructers_value) {
+  int val = 18;
+
+  SharedPtr<int> sharedPtr1(&val);
+  SharedPtr<int> sharedPtr2(sharedPtr1);
+
+  EXPECT_EQ(*sharedPtr1, 18);
+  EXPECT_EQ(*sharedPtr2, 18);
+  EXPECT_EQ(sharedPtr1.get(), &val);
+  EXPECT_EQ(sharedPtr2.get(), &val);
+  EXPECT_EQ(sharedPtr1.use_count(), 2);
+  EXPECT_EQ(sharedPtr2.use_count(), 2);
+  EXPECT_TRUE(sharedPtr1);
+  EXPECT_TRUE(sharedPtr2);
+}
+
+TEST(Constructers, Constructers_struct) {
+  struct Values {
+    int val;
+  };
+  Values val1 = {18};
+  SharedPtr<Values> sharedPtr1(&val1);
+  SharedPtr<Values> sharedPtr2(sharedPtr1);
+
+  EXPECT_EQ(sharedPtr1->val, 18);
+  EXPECT_EQ(sharedPtr2->val, 18);
+  EXPECT_EQ(sharedPtr1.get(), &val1);
+  EXPECT_EQ(sharedPtr2.get(), &val1);
+  EXPECT_EQ(sharedPtr1.use_count(), 2);
+  EXPECT_EQ(sharedPtr2.use_count(), 2);
+  EXPECT_TRUE(sharedPtr1);
+  EXPECT_TRUE(sharedPtr2);
+}
+
+TEST(Constructers, Constructers_class) {
+  class Values {
+   public:
+    int val;
+  };
+  Values val1;
+  val1.val = 18;
+  SharedPtr<Values> sharedPtr1(&val1);
+  SharedPtr<Values> sharedPtr2(sharedPtr1);
+
+  EXPECT_EQ(sharedPtr1->val, 18);
+  EXPECT_EQ(sharedPtr2->val, 18);
+  EXPECT_EQ(sharedPtr1.get(), &val1);
+  EXPECT_EQ(sharedPtr2.get(), &val1);
+  EXPECT_EQ(sharedPtr1.use_count(), 2);
+  EXPECT_EQ(sharedPtr2.use_count(), 2);
+  EXPECT_TRUE(sharedPtr1);
+  EXPECT_TRUE(sharedPtr2);
+}
+
+TEST(Constructers, Constructers_vector) {
+  std::vector<int> val1 = {1, 18, 17};
+  SharedPtr<std::vector<int>> sharedPtr1(&val1);
+  SharedPtr<std::vector<int>> sharedPtr2(sharedPtr1);
+
+  std::vector<int> val_v = {{1, 18, 17}};
+
+  EXPECT_EQ(*sharedPtr1, val_v);
+  EXPECT_EQ(*sharedPtr2, val_v);
+  EXPECT_EQ(sharedPtr1.get(), &val1);
+  EXPECT_EQ(sharedPtr2.get(), &val1);
+  EXPECT_EQ(sharedPtr1.use_count(), 2);
+  EXPECT_EQ(sharedPtr2.use_count(), 2);
+  EXPECT_TRUE(sharedPtr1);
+  EXPECT_TRUE(sharedPtr2);
+}
+
+TEST(Accessory_methods, Get) {
+  int val = 18;
+
+  SharedPtr<int> sharedPtr1(&val);
+  SharedPtr<int> sharedPtr2(sharedPtr1);
+
+  EXPECT_EQ(sharedPtr1.get(), &val);
+  EXPECT_EQ(sharedPtr2.get(), &val);
+}
+
+TEST(Accessory_methods, Count) {
+  int val = 18;
+
+  SharedPtr<int> sharedPtr1(&val);
+  SharedPtr<int> sharedPtr2(sharedPtr1);
+  SharedPtr<int> sharedPtr3(sharedPtr2);
+
+  EXPECT_EQ(sharedPtr1.use_count(), 3);
+  EXPECT_EQ(sharedPtr2.use_count(), 3);
+  EXPECT_EQ(sharedPtr3.use_count(), 3);
+}
+
+TEST(Accessory_methods, Bool) {
+int val = 18;
+
+SharedPtr<int> sharedPtr1(&val);
+SharedPtr<int> sharedPtr2(sharedPtr1);
+
+  EXPECT_TRUE(sharedPtr1);
+  EXPECT_TRUE(sharedPtr2);
+  EXPECT_FALSE(!sharedPtr1);
+  EXPECT_FALSE(!sharedPtr2);
+}
+
+TEST(Accessory_methods, Swap) {
+  int val1 = 18;
+
+  SharedPtr<int> sharedPtr1(&val1);
+  SharedPtr<int> sharedPtr2(sharedPtr1);
+  EXPECT_EQ(sharedPtr2.use_count(), 2);
+  EXPECT_EQ(sharedPtr2.get(), &val1);
+  EXPECT_EQ(*sharedPtr2, 18);
+  EXPECT_TRUE(sharedPtr2);
+
+  int val2 = 8;
+
+  SharedPtr<int> sharedPtr3(&val2);
+  SharedPtr<int> sharedPtr4(sharedPtr3);
+  SharedPtr<int> sharedPtr5(sharedPtr4);
+  EXPECT_EQ(sharedPtr5.use_count(), 3);
+  EXPECT_EQ(sharedPtr5.get(), &val2);
+  EXPECT_EQ(*sharedPtr5, 8);
+  EXPECT_TRUE(sharedPtr5);
+
+  sharedPtr5.swap(sharedPtr2);
+  EXPECT_EQ(sharedPtr2.use_count(), 3);
+  EXPECT_EQ(sharedPtr5.use_count(), 2);
+  EXPECT_EQ(sharedPtr2.get(), &val2);
+  EXPECT_EQ(sharedPtr5.get(), &val1);
+  EXPECT_EQ(*sharedPtr2, 8);
+  EXPECT_EQ(*sharedPtr5, 18);
+  EXPECT_TRUE(sharedPtr2);
+  EXPECT_TRUE(sharedPtr5);
+}
+
+class is_not_constructible {
+ public:
+  is_not_constructible& operator= (is_not_constructible&) = delete;
+};
+TEST(Exceptions, No_Copiable) {
+  try {
+    is_not_constructible object;
+    SharedPtr<is_not_constructible> sharedPtr1(&object);
+    SharedPtr<is_not_constructible> sharedPtr2(sharedPtr1);
+  } catch (std::runtime_error& e) {
+    EXPECT_EQ(e.what(), "Type not constructible.");
+  }
+}
+
+class is_not_assignable {
+ public:
+  is_not_assignable& operator= (is_not_assignable&&) = delete;
+};
+TEST(Exceptions, No_Assignable) {
+  try {
+    is_not_assignable object;
+    SharedPtr<is_not_assignable> sharedPtr1(&object);
+    SharedPtr<is_not_assignable> sharedPtr2;
+  } catch (std::runtime_error& e) {
+    EXPECT_EQ(e.what(), "Type not assignable.");
+  }
+}
+
+TEST(Exceptions, No_Copiable_equaling_operator) {
+  try {
+    is_not_constructible object;
+    SharedPtr<is_not_constructible> sharedPtr1(&object);
+    SharedPtr<is_not_constructible> sharedPtr2 = sharedPtr1;
+  } catch (std::runtime_error& e) {
+    std::string error = "Type not constructible.";
+    EXPECT_EQ(e.what(), error);
+  }
+}
+
+TEST(Exceptions, No_Assignable_equaling_operator) {
+  try {
+    is_not_assignable object;
+    SharedPtr<is_not_assignable> sharedPtr1(&object);
+    SharedPtr<is_not_assignable> sharedPtr2 = std::move(sharedPtr1);
+  } catch (std::runtime_error& e) {
+    std::string error = "Type not assignable.";
+    EXPECT_EQ(e.what(), error);
+  }
+}
